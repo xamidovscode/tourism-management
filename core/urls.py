33bb6a2c400777f5aed247337a8a62e9bdd44cf3
Dilmurod <eshmamatovdilmurod7@gmail.com>
2025-07-02 11:11:08 +0500

    new

--- core/urls.py
+++ core/urls.py
@@ -2,15 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 from apps.sales.views import export_pdf, export_excel
 from core.admin import admin_site
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
+
     path('admin/', admin.site.urls),
     # path('admin/', admin_site.urls),
     path('test/export-pdf/<int:pk>/', export_pdf, name='export_pdf'),
     path('test/export-excel/<int:pk>/', export_excel, name='export_excel'),
+    # path('i18n/', include('django.conf.urls.i18n')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
